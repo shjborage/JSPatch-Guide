8287b00829421f420665ff0d65e365302446c885
Eric <shjborage@gmail.com>
2016-03-29 17:18:21 +0800

    Add JSC Demo
    empty demo

---
+++ JavaScriptCore/JavaScriptCoreDemo/JavaScriptCoreDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JavaScriptCoreDemo
+//
+//  Created by shihaijie on 3/29/16.
+//  Copyright © 2016 Saick. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
